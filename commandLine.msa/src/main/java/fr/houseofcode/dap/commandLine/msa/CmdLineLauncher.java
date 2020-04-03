733ae4ab6cb4467c5169b0c46ad184f054a9ed94
djer13 <djer13@gmail.com>
2020-04-03 18:51:08 +0200

    Remarques sur le code

--- commandLine.msa/src/main/java/fr/houseofcode/dap/commandLine/msa/CmdLineLauncher.java
+++ commandLine.msa/src/main/java/fr/houseofcode/dap/commandLine/msa/CmdLineLauncher.java
@@ -14,80 +14,85 @@ import org.apache.logging.log4j.Logger;
  */
 public class CmdLineLauncher {
 
-	/**
-	 * Logger.
-	 */
-	private static final Logger LOG = LogManager.getLogger();
+    //TODO MSA by Djer |JavaDoc| On met en générale ces commentaires JavaDoc sur une seul ligne (je te l'ai modifié pour l'exemple)
+    /** Logger. */
+    private static final Logger LOG = LogManager.getLogger();
 
-	/**
-	 * Launch GmailService and CalendarService at the same time but in different
-	 * cases.
-	 * 
-	 * @param args is the external parameters
-	 * @throws IOException              if the sent or received message's broken
-	 * @throws GeneralSecurityException in case there's a security failure
-	 * @throws URISyntaxException
-	 */
-	public static void main(final String[] args) throws IOException, GeneralSecurityException, URISyntaxException {
-		ServerUtil su = new ServerUtil();
-		LOG.debug("Début du main avec comme arguments : " + args + "\n");
+    /**
+     * Launch GmailService and CalendarService at the same time but in different
+     * cases.
+     * 
+     * @param args is the external parameters
+     * @throws IOException              if the sent or received message's broken
+     * @throws GeneralSecurityException in case there's a security failure
+     * @throws URISyntaxException
+     */
+    public static void main(final String[] args) throws IOException, GeneralSecurityException, URISyntaxException {
+        ServerUtil su = new ServerUtil();
+        //TODO MSA by Djer |IDE| Attention les fichiers source JAVA devraient être encodés en UTF-8. Tu as laissé la valeur par defaut (qui est celle de ton OS : ISO-8859-1). Dans Eclipse Window->Preferences->General->Content Type, puis "text", puis "java source file". Tu peux aussi via Help->Perform Setup Task (que tu devrais lancer après l'installation de ton Eclipse) 
+        LOG.debug("Début du main avec comme arguments : " + args + "\n");
 
-		System.out.println("Que voulez-vou faire ? \n");
-		System.out.println("1 ==> afficher les labels");
-		System.out.println("2 ==> afficher les emails non lus");
-		System.out.println("3 ==> afficher le next event");
-		System.out.println("4 ==> afficher les labels, les emails non lus et le next event");
-		System.out.println("5 ==> ajouter un nouvel utilisateur \n");
-		System.out.println("Entrer votre choix :");
-		Scanner scanner = new Scanner(System.in);
-		Integer choixUserEntier = scanner.nextInt();
-		scanner.nextLine();
+        System.out.println("Que voulez-vou faire ? \n");
+        System.out.println("1 ==> afficher les labels");
+        System.out.println("2 ==> afficher les emails non lus");
+        System.out.println("3 ==> afficher le next event");
+        System.out.println("4 ==> afficher les labels, les emails non lus et le next event");
+        System.out.println("5 ==> ajouter un nouvel utilisateur \n");
+        System.out.println("Entrer votre choix :");
+        Scanner scanner = new Scanner(System.in);
+        Integer choixUserEntier = scanner.nextInt();
+        scanner.nextLine();
 
-		System.out.println("Entrer le nom d'utilisateur :");
-		String choixUserKey = scanner.nextLine();
+        System.out.println("Entrer le nom d'utilisateur :");
+        String choixUserKey = scanner.nextLine();
 
-		String labels = su.getLabels(choixUserKey);
-		String nbEmails = su.getNbUnreadEmail(choixUserKey);
-		String nextEvent = su.nextEvent(choixUserKey);
-		String addAccount = su.addAccount(choixUserKey);
+        String labels = su.getLabels(choixUserKey);
+        String nbEmails = su.getNbUnreadEmail(choixUserKey);
+        String nextEvent = su.nextEvent(choixUserKey);
+        String addAccount = su.addAccount(choixUserKey);
 
-		LOG.info("Utilisation d'un switch case pour les différents choix possible pour l'utilisateur");
-		try {
-			switch (choixUserEntier) {
-			case 1:
-				LOG.debug("Connexion d'utilisateur dans ses labels : ");
-				System.out.println("Here are your Labels : " + "\n" + labels);
-				break;
-			case 2:
-				LOG.debug("Connexion d'utilisateur dans sa boite mail ");
-				System.out.println("Vous avez : " + nbEmails + " emails non lus");
-				break;
-			case 3:
-				LOG.debug("Connexion d'utilisateur dans son GoogleCalendar : ");
-				System.out.println("Your next event is : " + nextEvent);
-				break;
-			case 4:
-				LOG.debug("Connexion d'utilisateur dans ses labels : ");
-				LOG.debug("Connexion d'utilisateur dans sa boite mail: ");
-				LOG.debug("Connexion d'utilisateur dans son GoogleCalendar : ");
-				System.out.println(" Here are your Labels : " + "\n" + labels);
-				System.out.println("Vous avez : " + nbEmails + " emails non lus");
-				System.out.println("Your next event is : " + nextEvent);
-				break;
-			case 5:
-				LOG.debug("Ajout d'un nouveau utilisateur et connexion au compte google : ");
-				System.out.println("Entrer un nouvel utilisateur : ");
-				String userToAdd = scanner.nextLine();
-				su.addAccount(userToAdd);
-				System.out.println("L'utilisateur : " + addAccount + "a été ajouté");
-				break;
-			default:
-				System.out.println("Pas d'option reconnue");
-			}
-		} catch (URISyntaxException e) {
-			System.out.println("Problem with URI syntax");
-		}
+        LOG.info("Utilisation d'un switch case pour les différents choix possible pour l'utilisateur");
+        try {
+            switch (choixUserEntier) {
+            case 1:
+                //TODO MSA by Djer |Log4J| Cette log est produite au mauvais moment, la "connexion" se fait ligne 50 ; Ici c'est juste l'affichage.
+                LOG.debug("Connexion d'utilisateur dans ses labels : ");
+                System.out.println("Here are your Labels : " + "\n" + labels);
+                break;
+            case 2:
+                //TODO MSA by Djer |Log4J| Cette log est produite au mauvais moment, la "connexion" se fait ligne 51 ; Ici c'est juste l'affichage.
+                LOG.debug("Connexion d'utilisateur dans sa boite mail ");
+                System.out.println("Vous avez : " + nbEmails + " emails non lus");
+                break;
+            case 3:
+                //TODO MSA by Djer |Log4J| Cette log est produite au mauvais moment, la "connexion" se fait ligne 52 ; Ici c'est juste l'affichage.
+                LOG.debug("Connexion d'utilisateur dans son GoogleCalendar : ");
+                System.out.println("Your next event is : " + nextEvent);
+                break;
+            case 4:
+                //TODO MSA by Djer |Log4J| Cette log est produite au mauvais moment, la "connexion" se fait ligne 50, 51, 52 ; Ici c'est juste l'affichage.
+                LOG.debug("Connexion d'utilisateur dans ses labels : ");
+                LOG.debug("Connexion d'utilisateur dans sa boite mail: ");
+                LOG.debug("Connexion d'utilisateur dans son GoogleCalendar : ");
+                System.out.println(" Here are your Labels : " + "\n" + labels);
+                System.out.println("Vous avez : " + nbEmails + " emails non lus");
+                System.out.println("Your next event is : " + nextEvent);
+                break;
+            case 5:
+                LOG.debug("Ajout d'un nouveau utilisateur et connexion au compte google : ");
+                System.out.println("Entrer un nouvel utilisateur : ");
+                String userToAdd = scanner.nextLine();
+                su.addAccount(userToAdd);
+                System.out.println("L'utilisateur : " + addAccount + "a été ajouté");
+                break;
+            default:
+                System.out.println("Pas d'option reconnue");
+            }
+        } catch (URISyntaxException e) {
+            //TODO MSA by Djer |Log4J| Une log d'erreur ?
+            System.out.println("Problem with URI syntax");
+        }
 
-		scanner.close();
-	}
+        scanner.close();
+    }
 }
--- commandLine.msa/src/main/java/fr/houseofcode/dap/commandLine/msa/ServerUtil.java
+++ commandLine.msa/src/main/java/fr/houseofcode/dap/commandLine/msa/ServerUtil.java
@@ -18,114 +18,122 @@ import org.apache.logging.log4j.Logger;
  */
 public class ServerUtil {
 
-	/**
-	 * Access to constant LOG.
-	 */
-	private static final Logger LOG = LogManager.getLogger();
+    /**
+     * Access to constant LOG.
+     */
+    private static final Logger LOG = LogManager.getLogger();
 
-	/**
-	 * String user_agent.
-	 */
-	private final static String USER_AGENT = "Mozilla/5.0";
+    //TODO MSA by Djer |JavaDoc| "User_agent used by internal browser to query Server API" serait plus juste.
+    /**
+     * String user_agent.
+     */
+    private final static String USER_AGENT = "Mozilla/5.0";
 
-	/**
-	 * Loading a web page to add a new user.
-	 * 
-	 * @param userKey
-	 * @throws IOException
-	 * @throws URISyntaxException
-	 */
-	public void addUser(final String userKey) throws IOException, URISyntaxException {
-		String addUser = "http://localhost:8081/user/add?name=" + userKey;
-		loadingNavigatorPage(addUser);
-	}
+    //TODO MSA by Djer |POO| Cette pas méthode n'est pas/plus utilisée.
+    /**
+     * Loading a web page to add a new user.
+     * 
+     * @param userKey
+     * @throws IOException
+     * @throws URISyntaxException
+     */
+    public void addUser(final String userKey) throws IOException, URISyntaxException {
+        String addUser = "http://localhost:8081/user/add?name=" + userKey;
+        loadingNavigatorPage(addUser);
+    }
 
-	/**
-	 * Add user account.
-	 * 
-	 * @param userKey
-	 * @return
-	 * @throws IOException
-	 * @throws URISyntaxException
-	 */
-	public String addAccount(final String userKey) throws IOException, URISyntaxException {
-		String addAccount = "http://localhost:8081/account/add/" + userKey;
-		loadingNavigatorPage(addAccount);
-		return addAccount;
-	}
+    /**
+     * Add user account.
+     * 
+     * @param userKey
+     * @return
+     * @throws IOException
+     * @throws URISyntaxException
+     */
+    public String addAccount(final String userKey) throws IOException, URISyntaxException {
+        String addAccount = "http://localhost:8081/account/add/" + userKey;
+        loadingNavigatorPage(addAccount);
+        return addAccount;
+    }
 
-	private void loadingNavigatorPage(final String account) throws IOException, URISyntaxException {
-		Desktop.getDesktop().browse(new URI(account));
-	}
+    //TODO MSA by Djer |JavaDoc| JavaDoc manquante.
+    //TODO MSA by Djer |POO| Le paramètre pourrait s'appeler "url" ou "urlToOpen" au lieu de "account".
+    private void loadingNavigatorPage(final String account) throws IOException, URISyntaxException {
+        Desktop.getDesktop().browse(new URI(account));
+    }
 
-	/**
-	 * Get labels function.
-	 * 
-	 * @param userKey
-	 * @return Labels
-	 */
-	public String getLabels(final String userKey) throws IOException {
-		String getLabels = callserver("/Email/Label", userKey);
-		return getLabels;
-	}
+    /**
+     * Get labels function.
+     * 
+     * @param userKey
+     * @return Labels
+     */
+    public String getLabels(final String userKey) throws IOException {
+        String getLabels = callserver("/Email/Label", userKey);
+        return getLabels;
+    }
 
-	/**
-	 * Get the next event.
-	 * 
-	 * @param userKey
-	 * @return nextEvent
-	 */
-	public String nextEvent(final String userKey) throws IOException {
-		String nextEvent = callserver("/event/next", userKey);
-		return nextEvent;
-	}
+    /**
+     * Get the next event.
+     * 
+     * @param userKey
+     * @return nextEvent
+     */
+    public String nextEvent(final String userKey) throws IOException {
+        String nextEvent = callserver("/event/next", userKey);
+        return nextEvent;
+    }
 
-	/**
-	 * Get the number of unread emails.
-	 * 
-	 * @param userKey
-	 * @return NbUnreadEmail
-	 */
-	public String getNbUnreadEmail(final String userKey) throws IOException {
-		String NbUnreadEmail = callserver("/Email/Unread", userKey);
-		return NbUnreadEmail;
+    /**
+     * Get the number of unread emails.
+     * 
+     * @param userKey
+     * @return NbUnreadEmail
+     */
+    public String getNbUnreadEmail(final String userKey) throws IOException {
+        String NbUnreadEmail = callserver("/Email/Unread", userKey);
+        return NbUnreadEmail;
 
-	}
+    }
 
-	/**
-	 * Call the server.
-	 * 
-	 * @param url, userKey
-	 * @return Response
-	 */
-	public String callserver(final String url, final String userKey) throws IOException {
+    /**
+     * Call the server.
+     * 
+     * @param url, userKey
+     * @return Response
+     */
+    public String callserver(final String url, final String userKey) throws IOException {
 
-		// HTTP GET request
-		URL obj = new URL("http://localhost:8081" + url + "?userKey=" + userKey);
-		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        //TODO MSA by Djer |POO| "http://localhost:8081" devrait être une variable local (pour éviter de repeter cette chaine ligne 119). Voir un élément configurable. 
+        // HTTP GET request
+        URL obj = new URL("http://localhost:8081" + url + "?userKey=" + userKey);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
 
-		// optional default is GET
-		con.setRequestMethod("GET");
+        // optional default is GET
+        con.setRequestMethod("GET");
 
-		// add request header
-		con.setRequestProperty("User-Agent", USER_AGENT);
+        // add request header
+        con.setRequestProperty("User-Agent", USER_AGENT);
 
-		int responseCode = con.getResponseCode();
-		System.out.println("\nSending 'GET' request to URL : " + "http://localhost:8081/" + url);
-		System.out.println("Response Code : " + responseCode);
+        int responseCode = con.getResponseCode();
+        //TODO MSA by Djer |Log4J| Devrait être une LOG. En Debug ? 
+        System.out.println("\nSending 'GET' request to URL : " + "http://localhost:8081/" + url);
+        //TODO MSA by Djer |Log4J| Devrait être une LOG. En Debug ? 
+        System.out.println("Response Code : " + responseCode);
 
-		BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-		String inputLine;
-		StringBuffer response = new StringBuffer();
+        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
 
-		while ((inputLine = in.readLine()) != null) {
-			response.append(inputLine);
-		}
-		in.close();
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
 
-		// print result
-		System.out.println(response.toString());
+        //TODO MSA by Djer |Log4J| Devrait être une LOG. En Debug ? 
+        // print result
+        System.out.println(response.toString());
 
-		return response.toString();
-	}
+        return response.toString();
+    }
 }
